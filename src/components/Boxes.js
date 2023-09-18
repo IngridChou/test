c5278e4b100e286fa2da530236faf2a2f0956de0
ingridchou <113267284+IngridChou@users.noreply.github.com>
2023-09-18 18:16:39 +0000

    Update Boxes.js

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,9 +1,12 @@
 import "./boxes.css";
 
 function Boxes() {
+    function handleinnerClick(event) {
+        alert{"You clicked the inner box"};
+    }
   return (
     <div className="outer">
-      <div className="inner"></div>
+      <div className="inner" onClick={handleInnerClick}></div>
     </div>
   );
 }
